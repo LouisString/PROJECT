5b66d3c26dcd93f3a642171bcb013f3cb37f9255
LouisString <cytokine@sohu.com>
2017-11-19 18:18:19 +0800

    REST Save

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import {Dish} from '../shared/dish'
-import {DISHES} from '../shared/dishes'
+import { Dish } from '../shared/dish'
+import { DISHES } from '../shared/dishes'
 import { DishService } from '../services/dish.service';
 import { Comment } from '../shared/comment';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -29,6 +29,7 @@ export class DishdetailComponent implements OnInit {
   comment: Comment;
   commentForm: FormGroup;
   errMess: string;
+  dishcopy = null;
 
 
   formErrors = {
@@ -48,22 +49,29 @@ export class DishdetailComponent implements OnInit {
     }
   }
 
-   constructor(private dishservice: DishService,
+  constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location,
     private cb: FormBuilder,
     @Inject('BaseURL') private BaseURL) {
-      this.createForm();
-    }
+    this.createForm();
+  }
 
-    ngOnInit() {
-      
-      this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-      this.route.params
-        .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-        .subscribe(dish => {this.dish = dish; this.setPrevNext(dish.id);},
-          errmess => this.errMess = <any>errmess);
-    } 
+  ngOnInit() {
+
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params
+      .switchMap((params: Params) => {
+        return this.dishservice.getDish
+          (+params['id']);
+      })
+      .subscribe(dish => {
+        this.dish = dish; this.dishcopy = dish; this
+          .setPrevNext(dish.id);
+      },
+      errmess => { this.dish = null; this.errMess = <any>errmess; });
+
+  } 
 
     createForm() {
       this.commentForm = this.cb.group({
@@ -99,10 +107,11 @@ export class DishdetailComponent implements OnInit {
   
     onSubmit() {
       this.comment = this.commentForm.value;
-      var d = new Date();
-      var date = d.toISOString();
-      this.comment.date = date;
-      DISHES[this.dish.id].comments.push(this.comment);
+      this.comment.date = new Date().toISOString();
+      console.log(this.comment);
+      this.dishcopy.comments.push(this.comment);
+      this.dishcopy.save()
+      .subscribe(dish => { this.dish = dish; console.log(this.dish); });
 
       this.commentForm.reset({
         author: '',
@@ -110,6 +119,7 @@ export class DishdetailComponent implements OnInit {
         rating: 5,
         date: ''
       });
+
 
     }  
 
